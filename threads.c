0b3bbaa350619b8612d77c499baffd680d4069a3
Connor Beshears <connorbeshears@gmail.com>
2019-10-04 09:16:13 -0500

    updated 10/3

--- threads.c
+++ threads.c
@@ -4,11 +4,18 @@
 void * my_thread(void* args);
 
 int main(){
-    pthread_t tid; //thread object
-    int label = 1;
+    const int THREADS = 3;
+    pthread_t tids[THREADS]; //thread arrays
+    int label[THREADS]; 
     void * result; //space for thread return
-    pthread_create(&tid, NULL, my_thread, (void *) &label); // create thread with pointer to thread, args, function, function args
-    pthread_join(tid, &result); // tells main to wait until thread is done
+    for(int i = 0; i<THREADS; i++){
+        label[i] = i;
+        pthread_create(&tids[i], NULL, my_thread, (void *) &label[i]); // create thread with pointer to thread, args, function, function args
+    }
+    for(int i = 0; i<THREADS; i++){
+        pthread_join(tids[i], &result); // tells main to wait until thread is done
+    }
+    printf("all done!\n");
     return 0;
 }
 
